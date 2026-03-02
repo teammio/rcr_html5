c5ed53be90b490d2caf89fc16a000c1a01d5460d
Xx_Dark_D_xX <89721511+mizu-mio@users.noreply.github.com>
2026-03-03 01:04:20 +0800

    commit.0

---
+++ rcr_html5/wph_utils_html5.js
@@ -0,0 +1,9 @@
+/* began */
+
+/// @description 占位符函数
+function window_enable_borderless_fullscreen(_flag) {};
+
+/// @description 占位符函数
+function display_get_frequency() { return(Infinity); };
+
+/* ended */
